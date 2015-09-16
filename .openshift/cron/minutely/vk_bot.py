7de5c73f4355771ac952dccba4e0edf83f9e3de8
Seryiza <seryiza@yandex.ru>
2015-09-16 20:59:37 +1000

    Test posting with cron in Mongo

---
+++ .openshift/cron/minutely/vk_bot.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import vk
+from pymongo import MongoClient
+import os
+
+mongo_db_url = os.environ['OPENSHIFT_MONGODB_DB_URL']
+
+client = MongoClient(mongo_db_url)
+db = client["python"]
+coll = db["testing"]
+
+import random
+coll.insert_one(random.random())
