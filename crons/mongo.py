0d82fede701c9830d6eab044b6e385d0d67114c3
Seryiza <seryiza@yandex.ru>
2015-09-17 21:33:21 +1000

    WIP

--- crons/mongo.py
+++ crons/mongo.py
@@ -1,7 +1,7 @@
 import pymongo
 import os
 
-mongo_uri = os.eniron['OPENSHIFT_MONGODB_DB_URL']
+mongo_uri = os.environ['OPENSHIFT_MONGODB_DB_URL']
 client = pymongo.MongoClient(mongo_uri)
 db = client['python']
 print(db.collection_names())
