5fddfbb599b5e3935d1194f9dcc1999f82235b92
Seryiza <seryiza@yandex.ru>
2015-09-19 20:10:57 +1000

    WIP

--- crons/mongo.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-import os
-
-mongo_uri = os.environ['OPENSHIFT_MONGODB_DB_URL']
-client = pymongo.MongoClient(mongo_uri)
-db = client['python']
-colll = db['test']
-post_id = colll.insert_one("hello").inserted_id
-print(post_id)
